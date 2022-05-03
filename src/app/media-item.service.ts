1ce06d8761a7b9e83006cc49ec2f067cdb2d371d
mitesh1 <bishtmitesh@gmail.com>
2022-05-04 01:27:55 +0530

    update on http get

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,57 +1,69 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MediaItemService {
+  constructor(private http: HttpClient) {}
+
+  get(medium) {
+    let getOptions = {
+      params: medium
+    }
+   return this.http.get<MediaItemResponse>('mediaitems', getOptions)
+   .pipe(map(response => response.mediaItems))
+  }
   mediaItems = [
     {
       id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
       year: 2010,
       watchedOn: 1294166565384,
-      isFavorite: false
+      isFavorite: false,
     },
     {
       id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
       year: 2015,
       watchedOn: null,
-      isFavorite: true
-    }, {
+      isFavorite: true,
+    },
+    {
       id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
       year: 2016,
       watchedOn: null,
-      isFavorite: false
-    }, {
+      isFavorite: false,
+    },
+    {
       id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
       year: null,
       watchedOn: null,
-      isFavorite: true
-    }, {
+      isFavorite: true,
+    },
+    {
       id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
       year: 2015,
       watchedOn: 1457166565384,
-      isFavorite: false
-    }
+      isFavorite: false,
+    },
   ];
 
-  get() {
-    return this.mediaItems;
-  }
+  
 
   add(mediaItem) {
     this.mediaItems.push(mediaItem);
@@ -64,3 +76,17 @@ export class MediaItemService {
     }
   }
 }
+
+interface MediaItem {
+  id: number;
+  name: string;
+  medium: string;
+  category: string;
+  year: number;
+  watchedOn: number;
+  isFavorite: boolean;
+}
+
+interface MediaItemResponse {
+  mediaItems: MediaItem[]
+} ;
